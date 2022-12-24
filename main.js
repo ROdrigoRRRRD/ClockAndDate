a7e5c6bf233f3fdc03cb0d28f463e5847d232f26
ROdrigoRRRRD <Roddrigo2003cod@gmail.com>
2022-12-23 21:21:02 -0600

    LocalStorage

--- main.js
+++ main.js
@@ -64,4 +64,18 @@ switchButton.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     switchButton.classList.toggle('active');
 
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+
 })
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    switchButton.classList.add('active');
+} else {
+    document.body.classList.remove('dark');
+    switchButton.classList.remove('active');
+}
